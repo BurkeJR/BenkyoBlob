2a71e983c5bc90e257f6442187140bb926f4519a
John Burke <burkejr19@gcc.edu>
2022-11-13 14:16:36 -0500

    Initial Commit

---
+++ BenkyoBlob/BenkyoBlobApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenkyoBlobApp.swift
+//  BenkyoBlob
+//
+//  Created by John Burke on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct BenkyoBlobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BenkyoBlob/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BenkyoBlob
+//
+//  Created by John Burke on 11/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
