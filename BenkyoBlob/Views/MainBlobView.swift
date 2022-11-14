00ed4e8f1cecda5115383323bd11c5212eb23147
SoraHirasawa <FutabaHirasawa@gmail.com>
2022-11-14 14:33:25 -0500

    Added shadows to Navigation Icons to make them pop out more

--- BenkyoBlob/Views/MainBlobView.swift
+++ BenkyoBlob/Views/MainBlobView.swift
@@ -21,67 +21,7 @@ struct MainBlobView: View {
                         .ignoresSafeArea()
                         .offset(y: geo.size.width / geo.size.height > CGFloat(0.58) ? -geo.size.height / 4 : 0)
                     VStack {
-                        HStack {
-                            NavigationLink {
-                                
-                            } label: {
-                                Image("ui-01-study-time-light-lrg")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-                            }
-                            //.padding()
-                            
-                            //Spacer()
-                            
-                            NavigationLink {
-                                
-                            } label: {
-                                Image("ui-03-quiz-light-lrg")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-
-                            }
-                            //.padding()
-                            
-                            //Spacer()
-                            
-                            NavigationLink {
-                                
-                            } label: {
-                                Image("ui-05-unlockables-light-lrg")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-
-                            }
-                            //.padding()
-                            
-                            NavigationLink {
-                                
-                            } label: {
-                                Image("ui-04-calendar-light-lrg")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-
-                            }
-                            //.padding()
-                            
-                            //Spacer()
-                            
-                            NavigationLink(tag: 3, selection: $modeSelect) {
-                                FlashcardsView(modeSelect: $modeSelect)
-                            } label: {
-                                Image("ui-02-flashcard-light-lrg")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50, height: 50)
-                            }
-                            //.padding()
-                        }
-                        .offset(y: -geo.size.height / 3)
+                        
                         //Spacer()
                         
                         Image("slimepet-01-pink-02-sad-lrg")
@@ -90,6 +30,67 @@ struct MainBlobView: View {
                             .offset(y: geo.size.width / geo.size.height > CGFloat(0.51) ? -geo.size.height / 10: geo.size.height / 16)
                         
                     }
+                    HStack {
+                        NavigationLink {
+                            
+                        } label: {
+                            Image("ui-01-study-time-light-with-colored-shadow-lrg")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                        }
+                        //.padding()
+                        
+                        //Spacer()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Image("ui-03-quiz-light-with-colored-shadow-lrg")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                            
+                        }
+                        //.padding()
+                        
+                        //Spacer()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Image("ui-05-unlockables-light-with-colored-shadow-lrg")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                            
+                        }
+                        //.padding()
+                        
+                        NavigationLink {
+                            
+                        } label: {
+                            Image("ui-04-calendar-light-with-colored-shadow-lrg")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                            
+                        }
+                        //.padding()
+                        
+                        //Spacer()
+                        
+                        NavigationLink(tag: 3, selection: $modeSelect) {
+                            FlashcardsView(modeSelect: $modeSelect)
+                        } label: {
+                            Image("ui-02-flashcard-light-with-colored-shadow-lrg")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 50, height: 50)
+                        }
+                        //.padding()
+                    }
+                    .offset(y: -geo.size.height / 1.75)
                     
                     
                 }
